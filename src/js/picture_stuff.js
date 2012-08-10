5d4b516cd15586a61757d4db6042156665d7bce5
Beth Brown <dramteke@cyrusinnovation.com>
2012-08-10 16:12:55 -0400

    Added a call to make a post on the facebook api.

--- src/js/picture_stuff.js
+++ src/js/picture_stuff.js
@@ -1,24 +1,46 @@
 $(document).ready(function () {
     // Taking and retrieving a picture
     $('#take-picture').tap(function () {
-        forge.file.getImage({source: 'camera'},onPhotoURISuccess);
+        getImageAndDisplay('camera');
     });
     $('#select-picture').tap(function () {
-        var width = parseInt($(window).width());
-
-        forge.file.getImage({source: 'gallery'}, function(file) {
-            forge.file.URL(file, function(url) {
-              jQT.goTo('#image-page');
-              var image = $('#myImage');
-              image.attr('style', 'width:' + width + 'px;');
-              image.attr('src', url);
-          });
-        });
-
+        getImageAndDisplay('gallery');
     });
 
     // Image page menu
     $('#share-fb').tap(function () {
-        alert('Share to Facebook!');
+        var title = "Mobile Upload"
+        var message = "My most recent picture brought to you by Cyrus Innovation!";
+        var photo_url = $('#myImage').attr('src');
+        postToFeed(title, message, photo_url);
     });
 });
+
+function getImageAndDisplay(sourceType){
+    var width = parseInt($(window).width());
+
+    forge.file.getImage({source: sourceType}, function(file) {
+        forge.file.URL(file, function(url) {
+            jQT.goTo('#image-page');
+            var image = $('#myImage');
+            image.attr('style', 'width:' + width + 'px;');
+            image.attr('src', url);
+        });
+    });
+}
+
+function postToFeed(title,msg,pht) {
+    // The Trigger.io file URL does not work when trying to upload a post to the fb feed
+    forge.facebook.ui(
+        {
+            method: 'feed',
+            //picture: pht,
+            picture: 'http://4.bp.blogspot.com/-8GmdKINnsgY/Tf8aLdRxJlI/AAAAAAAAAKY/9VCpycmEQr0/s1600/Sunset.jpg',
+            name: title,
+            caption: '',
+            description: msg
+        }, function(response){
+            alert('Posted!');
+        }
+    );
+}
